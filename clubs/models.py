83e43708d38e8d29ea1161b730bb217a1cc05c5f
Oleksii Sterlikov <Oleksii.Sterlikov@gmail.com>
2025-03-23 11:05:21 +0200

    Connected Django admin panel and changed models clubs

--- clubs/models.py
+++ clubs/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Club(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    url = models.URLField()
+    url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
