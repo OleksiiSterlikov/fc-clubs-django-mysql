5ec8314c2a3caf0ec62ec5e7f415202e072804d6
Oleksii Sterlikov <Oleksii.Sterlikov@gmail.com>
2025-03-30 23:20:05 +0300

    Add Form to the Clubs object and view details on id

--- clubs/urls.py
+++ clubs/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import add_club
+from django.views.generic import detail
+
+from .views import add_club, details_club
 
 
 urlpatterns = [
     path('/add', add_club, name='add_club'),
+    path('/<int:id>', details_club, name='details_club'),
 ]
--- players/views.py
+++ players/views.py
@@ -5,16 +5,18 @@ from .models import Player
 
 
 def add_player(request):
-    if request.method == 'GET':
-        clubs = Club.objects.all()
-        return render(request, "players/add-player.html", {'clubs': clubs})
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            clubs = Club.objects.all()
+            return render(request, "players/add-player.html", {'clubs': clubs})
+        else:
+            player = Player()
+            player.first_name = request.POST['first_name']
+            player.last_name = request.POST['last_name']
+            player.birth_date = request.POST['birth_date']
+            player.club = request.POST['club']
+            player.user = request.user
+            player.save()
+            return redirect('/')
     else:
-        player = Player()
-        player.first_name = request.POST['first_name']
-        player.last_name = request.POST['last_name']
-        player.birth_date = request.POST['birth_date']
-        player.club = request.POST['club']
-        player.user = request.user
-        player.save()
         return redirect('/')
-
