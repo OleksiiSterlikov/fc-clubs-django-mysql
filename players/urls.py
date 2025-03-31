28b8d5ed75e8ef084aa3a534a70edea6920f2893
Oleksii Sterlikov <Oleksii.Sterlikov@gmail.com>
2025-03-31 15:06:00 +0300

    Fixed add players and views on main page

--- players/urls.py
+++ players/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import add_player
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('add', add_player, name='add_player'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
