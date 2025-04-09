866b88e29ba3f32fb1032e03c81e26608788746f
Oleksii Sterlikov <Oleksii.Sterlikov@gmail.com>
2025-04-09 21:36:50 +0300

    Add functions Sign Up, Sign In and Logout

--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.conf import settings
+from django.contrib.auth import authenticate
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import MenuItem
 from clubs.models import Club
 from players.models import Player
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
 
 # Create your views here.
@@ -19,6 +22,32 @@ def home(request):
                   })
 def sign_up(request):
     if request.method == "POST":
-        pass
+        user = User()
+        user.username = request.POST['username']
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.set_password(request.POST['password'])
+        user.is_superuser = False
+        user.is_staff = False
+        user.is_active = True
+        user.save()
+        if user is not None:
+            login(request, user)
+        return redirect('/')
     else:
         return render(request, 'main/sign-up.html', {})
+
+def sign_in(request):
+    if request.method == "POST":
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+        return redirect('/')
+    else:
+        return render(request, 'main/sign-in.html', {})
+
+def logout_user(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('/')
