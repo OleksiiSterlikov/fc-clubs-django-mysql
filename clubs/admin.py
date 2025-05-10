ecc3ee0fd3dfdbc47674385db57590a61f18efee
Oleksii Sterlikov <Oleksii.Sterlikov@gmail.com>
2025-05-10 19:20:30 +0300

    Add models League and function and page add_league and fixing functions edit_club, update_club

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Club
+from .models import Club, League, LeagueClub
 
 # Register your models here.
 admin.site.register(Club)
+admin.site.register(League)
+admin.site.register(LeagueClub)
--- clubs/views.py
+++ clubs/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Club
+from .models import Club, League, LeagueClub
 
 
 def add_club(request):
     if request.user.is_authenticated:
-        if request.method == "GET":
-            return render(request, 'clubs/add-club.html')
+        if request.method == 'GET':
+            leagues = League.objects.all()
+            return render(request, 'clubs/add-club.html', {'leagues': leagues})
         else:
             club = Club()
             club.name = request.POST['name']
@@ -17,6 +18,11 @@ def add_club(request):
             if 'img_emblem' in request.FILES:
                 club.img_emblem = request.FILES['img_emblem']
             club.save()
+            for league in request.POST.getlist('leagues', []):
+                league_club = LeagueClub()
+                league_club.club = club
+                league_club.league = League.objects.get(id=int(league))
+                league_club.save()
             return redirect('/')
     else:
         return redirect('/')
@@ -25,14 +31,48 @@ def add_club(request):
 def edit_club(request, id):
     if request.user.is_authenticated:
         club = Club.objects.get(id=id)
-        return render(request, 'clubs/add-club.html', {'club': club})
+        leagues = League.objects.all()
+        club_leagues = LeagueClub.objects.filter(club_id=club.id)
+        return render(request, 'clubs/add-club.html', {
+            'club': club,
+            'club_leagues': club_leagues,
+            'leagues': leagues
+        })
     else:
         raise PermissionError
+
+
+def add_league(request):
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            return render(request, 'clubs/league/add-league.html')
+        else:
+            league = League()
+            league.title = request.POST['title']
+            league.description = request.POST['description']
+            if 'img_emblem' in request.FILES:
+                league.img_emblem = request.FILES['img_emblem']
+            league.save()
+            return redirect('/')
+    else:
+        return redirect('/')
 
 
 def update_club(request, id):
     if request.user.is_authenticated:
         club = Club.objects.get(id=id)
+        club.name = request.POST['name']
+        club.location = request.POST['location']
+        club.since_year = request.POST['since_year']
+        club.description = request.POST['description']
+        club.site_page = request.POST['site_page']
+        club.save()
+        LeagueClub.objects.filter(club_id=club.id).delete()
+        for league in request.POST.getlist('leagues', []):
+            league_club = LeagueClub()
+            league_club.club = club
+            league_club.league = League.objects.get(id=int(league))
+            league_club.save()
     return redirect('/')
 
 
